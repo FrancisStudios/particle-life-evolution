1b0a95b32df217621bc0a3efe089c3993a4bb2f9
Francis <vlfmap@gmail.com>
2025-11-11 06:58:13 +0100

    logger

---
+++ src/h/logger.h
@@ -0,0 +1,17 @@
+/**
+ *  ------ [ Property of Francis Studios ] ------
+ * ==============================================
+ * This is an open source project, feel free to use
+ * .. github:https://github.com/francisstudios ..
+ * ____ ©2025 Francis Studios Softwares by L. ___
+ */
+#ifndef LOGGER_H
+#define LOGGER_H
+#pragma once
+
+#include <stdio.h>
+namespace Logger
+{
+
+}
+#endif
---
+++ src/logger.cpp
@@ -0,0 +1,17 @@
+/**
+ *  ------ [ Property of Francis Studios ] ------
+ * ==============================================
+ * This is an open source project, feel free to use
+ * .. github:https://github.com/francisstudios ..
+ * ____ ©2025 Francis Studios Softwares by L. ___
+ */
+
+#include "./h/logger.h"
+
+namespace Logger
+{
+    void print(const char *message)
+    {
+        printf("%s", message);
+    }
+}
