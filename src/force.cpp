d5d338b48f601a2dd629a11360aba93237ab82ac
Francis <vlfmap@gmail.com>
2025-11-26 18:54:02 +0100

    force module added

---
+++ src/force.cpp
@@ -0,0 +1,14 @@
+/**
+ *  ------ [ Property of Francis Studios ] ------
+ * ==============================================
+ * This is an open source project, feel free to use
+ * .. github:https://github.com/francisstudios ..
+ * ____ ©2025 Francis Studios Softwares by L. ___
+ */
+
+#include "./h/force.h"
+
+namespace Force
+{
+
+}
---
+++ src/h/force.h
@@ -0,0 +1,19 @@
+/**
+ *  ------ [ Property of Francis Studios ] ------
+ * ==============================================
+ * This is an open source project, feel free to use
+ * .. github:https://github.com/francisstudios ..
+ * ____ ©2025 Francis Studios Softwares by L. ___
+ */
+
+#ifndef FORCE_H
+#define FORCE_H
+
+#pragma once
+
+namespace Force
+{
+
+}
+
+#endif
