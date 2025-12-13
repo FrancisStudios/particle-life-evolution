447c509df7d31bb84dcc6c8543557e8179710ff0
Francis <vlfmap@gmail.com>
2025-12-13 09:14:53 +0100

    v0.3.6

--- src/util.cpp
+++ src/util.cpp
@@ -8,7 +8,7 @@
 #include "./h/util.h"
 
 #define APP_NAME "Particle Life"
-#define APP_VERSION "v0.3.5"
+#define APP_VERSION "v0.3.6"
 #define AUTHOR "by FrancisStudios"
 #define SPACING " "
 #define DIVIDER " | "
